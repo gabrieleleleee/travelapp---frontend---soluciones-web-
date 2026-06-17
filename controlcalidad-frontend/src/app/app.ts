db394a62d6481ad0f3e31b9f3be8638495b06c0a
whoisguttixd <fabianpkm1@gmail.com>
2026-06-16 21:11:41 -0500

    Fixeo del frontend

--- controlcalidad-frontend/src/app/app.ts
+++ controlcalidad-frontend/src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { AuthService } from '../services/auth.service';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
